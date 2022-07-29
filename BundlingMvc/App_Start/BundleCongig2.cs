487d6ffd3dee75d2e9f9afd02be144b115c2b9f0
HP <HP@DESKTOP-G4J7E8F>
2022-07-29 18:37:57 +0530

    Add project files.

---
+++ BundlingMvc/App_Start/BundleCongig2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BundlingMvc.App_Start
+{
+    public class BundleCongig2
+    {
+        public static void RegisterBundle(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/cssStyle").Include("~/Content/Paragraphs.css", "~/Content/Headings.css"));
+
+
+        }
+    }
+}
---
+++ BundlingMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BundlingMvc.MvcApplication" Language="C#" %>
